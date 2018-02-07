bbcc39cffc667c3b79613fb5145793e38647a31e
Diana Borbe <dborbe@axway.com>
2018-02-07 11:07:26 -0500

    Make build.sh to handle app compilation and image creation

--- build.sh
+++ build.sh
@@ -2,4 +2,18 @@
 
 echo "-----Start executing build.sh-----"
 
-javac HelloWorld.java
+echo "-----Clean environment-----"
+docker system prune -f
+
+docker build . -f dockerfile-compile -t runner
+docker run --name runner-container --detach -t runner
+docker cp runner-container:/workspace/HelloWorld.class ./
+docker stop runner-container
+
+docker build . -f dockerfile-app -t app
+docker run --name app-container -t app
+
+# leave a clean environment
+docker system prune -f
+
+echo "-----Finished executing build.sh-----"
